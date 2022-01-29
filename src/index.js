70a18f7df06d6d2ac9d7cb067c16c1bd5bac6ce6
Mahdi Askarpoor <mahdiaskarpoor1383@gmail.com>
2022-01-29 21:00:44 +0330

    Edit 'index.js' : Add 'constructor()' method to class 'App'

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
+    // constructor() is optional
+    constructor(props) {
+        super(props); // React says we Must use super(props)
+        this.state = { lat: null }; //? define a state
+    }
+
+    // React says we Must use render()
     render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
             (err) => console.log(err)
         );
-        return <div> Latitude: </div>;
+        return <div> Latitude: {this.state.lat}</div>; //? show defined state
     }
 }
 
