2067e649772d440ef39f6dea6af8bde8204a373d
Mahdi Askarpoor <mahdiaskarpoor1383@gmail.com>
2022-01-29 14:27:52 +0330

    Edit 'index.js' : Add js codes to get 'UserLocation'

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err)
+    );
+    return <div>Hi There!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
