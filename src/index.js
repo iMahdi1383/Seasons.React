6314a407dd6b84249586e237aaf9a8dd484f2e1f
Mahdi Askarpoor <mahdiaskarpoor1383@gmail.com>
2022-02-02 22:22:58 +0330

    Edit 'index.js' : Add another state to show user location errors

--- src/index.js
+++ src/index.js
@@ -4,20 +4,21 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
     constructor(props) {
         super(props);
-
-        this.state = { lat: null }; //? Define a state
-
+        this.state = { lat: null, errorMessage: "" }; //#1 define state
         window.navigator.geolocation.getCurrentPosition(
-            // after get position, a callback function :
-            (position) => {
-                this.setState({ lat: position.coords.latitude }); //? Update state value, then Render Component again
-            },
-            (err) => console.log(err)
+            (position) => this.setState({ lat: position.coords.latitude }), //#2 update state
+            (err) => this.setState({ errorMessage: err.message })
         );
     }
 
     render() {
-        return <div> Latitude: {this.state.lat}</div>; //? Show state
+        return (
+            <div>
+                Latitude: {this.state.lat} {/*//#3 show state */}
+                <br />
+                Error: {this.state.errorMessage}
+            </div>
+        );
     }
 }
 
