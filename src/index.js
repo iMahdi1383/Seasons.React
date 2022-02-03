caef0bd39d30d1de9b1fc633205ba4b1cad94d3f
Mahdi Askarpoor <mahdiaskarpoor1383@gmail.com>
2022-02-03 12:42:28 +0330

    Edit 'index.js' : Add 'componentDidMount()' instead of get user location in 'constructor()'

--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
+    //#1  constructor : define variables
     constructor(props) {
         super(props);
-        this.state = { lat: null, errorMessage: "" }; //#1 define state
+        this.state = { lat: null, errorMessage: "" };
+    }
+
+    //#2  componentDidMount() : load data (after load page)
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => this.setState({ lat: position.coords.latitude }), //#2 update state
+            (position) => this.setState({ lat: position.coords.latitude }),
             (err) => this.setState({ errorMessage: err.message })
         );
     }
 
+    //#3  render() : just return JSX
     render() {
         if (this.state.lat && !this.state.errorMessage)
-            return <div> Latitude: {this.state.lat} </div>; //#3 show state
+            return <div> Latitude: {this.state.lat} </div>;
         if (!this.state.lat && this.state.errorMessage)
             return <div> Error: {this.state.errorMessage} </div>;
         return <div> Loading... Please wait </div>;
