e6ea15bdd098037b20e1f05f3480864160aab2a6
Mahdi Askarpoor <mahdiaskarpoor1383@gmail.com>
2022-02-04 13:53:29 +0330

    Edit 'SeasonDisplay.js' : Add functions 'seasonConfig(), getSeason(), seasonDisplay()'

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,60 @@
 import React from "react";
 
-const SeasonDisplay = () => {
-    return <div>Hi There!</div>;
+const seasonConfig = {
+    spring: {
+        text: "Nothing is so beautiful as Spring! 🌸",
+        iconName: "sun",
+    },
+    summer: {
+        text: "Let's hit the beach! 🏖️",
+        iconName: "life ring",
+    },
+    fall: {
+        text: "brown leaves falls on the ground! 🍁",
+        iconName: "umbrella",
+    },
+    winter: {
+        text: "Burr... Its Chilly! 🥶 ",
+        iconName: "snowflake",
+    },
+};
+
+const getSeason = (lat, month) => {
+    switch (month) {
+        case 0: // January
+        case 1: // February
+        case 2: // ...
+            return lat > 0 ? "winter" : "summer";
+        case 3:
+        case 4:
+        case 5:
+            return lat > 0 ? "spring" : "fall";
+        case 6:
+        case 7:
+        case 8:
+            return lat > 0 ? "summer" : "winter";
+        case 9: //  ...
+        case 10: // November
+        case 11: // December
+            return lat > 0 ? "fall" : "spring";
+        default:
+            return "Error : we cant get Date of your device";
+    }
+};
+
+const SeasonDisplay = (props) => {
+    const lat = props.lat;
+    const month = new Date().getMonth();
+    let season = getSeason(lat, month);
+    const { text, iconName } = seasonConfig[season];
+
+    return (
+        <div>
+            <i className={`icon ${iconName}`}></i>
+            <h1> {text} </h1>
+            <i className={`icon ${iconName}`}></i>
+        </div>
+    );
 };
 
 export default SeasonDisplay;
